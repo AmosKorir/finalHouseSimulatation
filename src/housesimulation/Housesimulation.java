f254a2b279c61321ca37214dfe920ea168184f9b
skyways <amoskrr@gmail.com>
2018-07-11 12:16:12 +0300

    final model

---
+++ src/housesimulation/Housesimulation.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package housesimulation;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Housesimulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("House Simulation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,500);
+        House house=new House();
+        
+        JPanel flower = new Flower();
+        flower.setBackground(Color.white);
+        frame.add(flower);
+        frame.add(house);
+        frame.setVisible(true);
+                
+        
+    }
+    
+}
